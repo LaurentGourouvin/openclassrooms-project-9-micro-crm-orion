089fcb134e06b80ff7554f66ee387332435e52ea
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-04-27 16:00:56 +0200

    chore: update config karma for test coverage

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -27,7 +27,8 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/microcrm"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [{ type: "html" }, { type: "text-summary" }, { type: 'lcovonly', file: 'lcov.info' }
+      ],
     },
     reporters: ["progress", "kjhtml"],
     browsers: ["ChromeHeadlessNoSandbox", "ChromeHeadless", "Chrome"],
